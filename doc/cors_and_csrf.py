786c3db16ffcb81fd32b5cf30abb862869e446c6
scott <24509826@qq.com>
2016-10-04 02:23:24 +0800

    update

--- doc/cors_and_csrf.py
+++ doc/cors_and_csrf.py
@@ -50,3 +50,22 @@ CORS_ALLOW_HEADERS = (
 )
 
 
+"""
+ajax使用django生成的csrf-token
+
+虽然开启了csrf
+ 'django.middleware.csrf.CsrfViewMiddleware',
+ 但实际上django的response并没有产生csrftoken返回到前端，
+ 这会影响前端ajax程序在发送POST,UPDATE,DELETE,PATCH无法发送有效的csrftoken而请求无效.
+ {% csrf_token %}通过form field传递的csrftoken另当别论。
+
+ 要解决这个问题，请见 django.middle.ware.csrf 的 process_response()处理函数，其检查
+  request.META.get("CSRF_COOKIE_USED") 是否设置，未设置则不会返回csrf的cookie
+
+ 1. get_token(request) 函数可以生成新的csrftoken的cookie，所以只需要调用一下get_token()即可
+ 2. 编写新的middleware，从 CsrfViewMiddleware派生， process_response()中调用get_token()即可。
+
+ajax使用csrftoken最简单的方式使用  restframework/js/csrf.js
+
+"""
+
