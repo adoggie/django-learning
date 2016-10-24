669407d1a3152a5f55d5d41f808af989cf67ba5c
scott <24509826@qq.com>
2016-10-25 00:29:51 +0800

    Update list_all.py

--- doc/third-party/list_all.py
+++ doc/third-party/list_all.py
@@ -33,6 +33,39 @@ pip install markdown2pdf
 
 http://wkhtmltopdf.org/
 
+“”“
+文件&目录/show/upload
 
+perfect_html  
+	https://pypi.python.org/pypi/perfect_html/0.0.6
+dirwalker  
+	https://pypi.python.org/pypi/dirwalker/0.5.0
+perfect_html 
+	https://pypi.python.org/pypi/perfect_html/0.0.6
+django-dirbrowser 
+	https://pypi.python.org/pypi/django-dirbrowser/0.0.3
+rdir 
+	https://pypi.python.org/pypi/rdir/0.52
+	
+	
+django-jqueryfileupload
+	https://pypi.python.org/pypi/django-jqueryfileupload/0.0.1
+
+django-chunked-upload
+	https://pypi.python.org/pypi/django-chunked-upload/1.1.1
+django-storages-s3upload
+		https://pypi.python.org/pypi/django-storages-s3upload/0.1.6
+danemco-uploader
+	https://pypi.python.org/pypi/danemco-uploader/1.2.0
+ajaxuploader 
+	https://pypi.python.org/pypi/ajaxuploader/0.3.8
+django-uploadstatic
+	https://pypi.python.org/pypi/django-uploadstatic/0.3.0
+django-uploader
+	https://pypi.python.org/pypi/django-uploader/0.2.1
+XStatic-jQuery-File-Upload 
+	https://pypi.python.org/pypi/XStatic-jQuery-File-Upload/9.7.0.1
+
+"""
 
 
