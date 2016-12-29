ff5d79de71aed495336588b520473f64b94ed63d
scott <24509826@qq.com>
2016-12-29 23:43:31 +0800

    Update linux_tools_teaching.py

--- doc/linux_tools_teaching.py
+++ doc/linux_tools_teaching.py
@@ -21,3 +21,84 @@ Postgresql
    create database testdb;
    alter user postgres with password "xxxx" 
    
+software installation
+--------------
+1. 不应该使用root安装应用软件，创建独立的用户和用户组，并设置对应的文件控制权限
+2. chroot
+3. docker
+
+   
+Tools
+----------
+
+yum: 
+   yumdownloader 
+   source files: /etc/yum.repos.d/xxxx 
+   yum install|search|remove|update
+   yum install epel-release 
+   
+
+command types:  1.builtin ; 2. external
+
+man 1,2,3,4,5
+echo / touch / source / export 
+getconf LONG_BIT
+cat /etc/release-***
+hostname / hostnamectl / uname -a / ulimit /sysctl -a 
+
+
+
+
+1. top /htop / free -m / df / du 
+2. vi/vim/nano/less/emacs
+   ".vimrc "
+3. wget/curl 
+4. setsid / nohup / > dev/null 2>&1
+5. tmux / screen 
+6. nc -l xxxx / nmap / netstat -an / lsof -i:8080 
+7. strings / tail -nf / head / watch -nd
+8. awk / sed / tr 
+9. pkill / pgrep / kill / ps 
+10.grep / find / xargs -l {}
+11. ssh / scp / sshfs 
+12. samba / nfs 
+13. http server :  httpd / nginx / mongoose / python -m SimpleHTTPServer 8080 
+14. iptables: 
+     service iptables start | stop | restart
+15. rsync -rvat  src dest 
+16. cron  (crontab -e) 
+17. svn / svnserve / 
+18. vnc-server 
+   yum install tigervnc-server 
+   vncserver --geometry 600x450 
+   vncviewer xxxxx:1 
+19. webssh : shellinaboxd 
+   
+   
+   
+"sshd  配置无需密码登录 "
+      vim /etc/ssh/ssh_config
+         StrictHostKeyChecking no
+      service sshd restart
+      
+      生成公私钥： ssh-keygen -t rsa  -> ~/.ssh/id_rsa.pub | id_rsa
+      在登录目标主机文件 ~/.ssh/authorized_keys 添加本机的公钥内容( id_rsa.pub) 
+      
+System Configurations:
+---------------
+/etc/resolv.conf 
+/etc/hosts
+/etc/hostname
+/etc/release-***
+
+
+
+Pythons
+--------
+pip / easy_install / requiments.txt 
+virtualenv 
+
+
+
+
+
