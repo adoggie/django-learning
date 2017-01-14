9e153b019d723c84d73aaf41d1820f8d9af091ab
scott <24509826@qq.com>
2017-01-15 01:49:30 +0800

    Update spark-pyspark.py

--- doc/spark-pyspark.py
+++ doc/spark-pyspark.py
@@ -9,3 +9,20 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("myApp") 
 sc = SparkContext(conf = conf)
+
+
+"""
+-- 统计客户通话记录时长 --
+
+a = sc.textFile('./sjt/a.txt')
+b = sc.textFile('./sjt/b.txt')
+
+
+rs = b.map( lambda s:s.split(',')).\
+	filter(lambda x: len(x)>5 ).\
+	map(lambda _:(_[0],_[5])).\
+	reduceByKey(lambda x,y: int(x)+ int(y) )
+rs.saveAsTextFile('result.txt')
+
+"""
+
