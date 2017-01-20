a913b55f4366ba76891711a7c333d049f7ec6442
scott <24509826@qq.com>
2017-01-20 22:55:33 +0800

    Update kafka_simple.py

--- doc/bigdata/kafka_simple.py
+++ doc/bigdata/kafka_simple.py
@@ -1,5 +1,24 @@
 
+启动zookeeper and kafka-server
 
+‘’‘’
 bin/zookeeper-server-start.sh config/zookeeper.properties
- bin/kafka-server-start.sh config/server.properties
+bin/kafka-server-start.sh config/server.properties
+‘’‘
+
+创建topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test 
+ 查看topic
+ bin/kafka-topics.sh --list --zookeeper localhost:2181
+  
+发送消息
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
  
+ 接收消息
+ bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
+  
+  
+   
+   
+  
+  
