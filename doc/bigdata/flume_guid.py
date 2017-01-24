71041e342fb6b48d9216e364e656a9ed6f28ac68
scott <24509826@qq.com>
2017-01-25 02:57:13 +0800

    Create flume_guid.py

---
+++ doc/bigdata/flume_guid.py
@@ -0,0 +1,18 @@
+
+"""
+
+http://flume.apache.org/releases/content/1.6.0/FlumeUserGuide.html
+
+source,sink,channel 
+
+
+
+"""
+
+"""
+kafka -> flume -> hdfs 
+flume -> kafka -> flume -> hdfs 
+
+
+"""
+
