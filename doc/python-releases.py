680a92aa645f40e71d64167a9839b70759c45774
scott <24509826@qq.com>
2017-02-19 15:57:21 +0800

    Update python-releases.py

--- doc/python-releases.py
+++ doc/python-releases.py
@@ -2,3 +2,11 @@
 https://docs.djangoproject.com/en/1.10/releases/
 https://www.djangoproject.com/download/
   
+
+backwards-incompatible-1-7
+
+django 1.7 必须手动添加 .setup() ,否则出现"AppRegisterNotReady" 的错误
+=========
+ >>> import django
+ >>> django.setup()
+  Otherwise, you will hit an AppRegistryNotReady exception.
