3f8d31754f8ab7a5672f119fbb99651b5a3915b8
scott <24509826@qq.com>
2017-05-05 22:16:03 +0800

    Update pgsql_server_command_howto.sh

--- doc/pgsql_server_command_howto.sh
+++ doc/pgsql_server_command_howto.sh
@@ -8,6 +8,28 @@ su - postgres -c " pg_ctl start|stop|status -D /var/lib/pgsql/9.4/data"
 pg_ctl -D /data/pgsql -l logfile start
 
 
+'''
+pwd=$(cd `dirname $0`;pwd)
+
+echo 'waiting for database is ok..'
+sleep 4
+echo 'create database lemon..'
+su - postgres -c "createdb lemon"
+echo 'alter user role..'
+su - postgres -c "psql -c \"alter user postgres with password '111111'\" "
+
+echo "create tables.."
+cd /opt/lemon/model/django
+python manage.py syncdb --noinput
+cd -
+sleep 1
+echo "init data scripts.."
+cd /opt/lemon/test
+python init_org_user.py
+cd -
+
+'''
+
 
 
 
