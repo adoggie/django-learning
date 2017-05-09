4a23c75d137a5c9ffcfca5189c9b98804ff7b3a7
scott <24509826@qq.com>
2017-05-09 13:16:51 +0800

    Update draw_simple.py

--- doc/graphviz/draw_simple.py
+++ doc/graphviz/draw_simple.py
@@ -1,2 +1,8 @@
 
+http://www.webgraphviz.com/
+http://graphviz.org/content/command-line-invocation
+http://graphviz.org/Gallery.php
+http://mg.pov.lt/objgraph/
+  
+
 objgraph.show_refs([c],filename='/tmp/test.png')
