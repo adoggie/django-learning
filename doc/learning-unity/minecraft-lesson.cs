c9a1af69389a6f41545216e1c851f4e0cf370776
scott <24509826@qq.com>
2018-01-09 18:57:31 +0800

    Create minecraft-lesson.cs

---
+++ doc/learning-unity/minecraft-lesson.cs
@@ -0,0 +1,8 @@
+
+向左转（角速度） 
+GetComponent<Rigidbody>().angularVelocity = new Vector3(0,-1f,0) 
+angularVelocity ＝ （0,0,0)  停止旋转 
+
+向前向后移动 
+GetComponent<RigidBody>().velocity = new Vector3(0,0,20)
+velocity = (0,0,0)  停止
