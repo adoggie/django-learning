eafdc78f93fa2093479345dc453dfd22e27725de
scott <24509826@qq.com>
2018-01-13 20:11:59 +0800

    Update minecraft-lesson.cs

--- doc/learning-unity/minecraft-lesson.cs
+++ doc/learning-unity/minecraft-lesson.cs
@@ -23,3 +23,10 @@ Partical 粒子显示block消失时的爆裂特效（puff）
 Constant Force 常数力
 - Force 常数恒力，不变的力
 - Relative Force  在 Update()使用，连续的施加力
+
+Update()刷新函数中设置速度
+gameObject.transform.Translate(Vector3.forward*speed*Time.deltaTime)   deltaTime - 帧刷新之间的时间间隔
+
+Raycast 射线检测 
+  RaycastHit hit
+  Physics.Raycast(origin,direction,out hit,length)    direction : 表示一个向量 , 可以由两个空间位置相减得到 , length : 向量的长度 Vector3.magnitude
