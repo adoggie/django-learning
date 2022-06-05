29c0a88542418490cb886d40d1e107a154490bfd
scott <socketref@hotmail.com>
2022-06-06 00:02:53 +0800

    Create array_split.go

---
+++ doc/golang/array_split.go
@@ -0,0 +1,19 @@
+func chunkSlice(slice []int, chunkSize int) [][]int {
+	var chunks [][]int
+	for {
+		if len(slice) == 0 {
+			break
+		}
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if len(slice) < chunkSize {
+			chunkSize = len(slice)
+		}
+
+		chunks = append(chunks, slice[0:chunkSize])
+		slice = slice[chunkSize:]
+	}
+
+	return chunks
+}
