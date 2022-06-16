ca194677ad7286de93fea87a029940abe7b54781
scott <socketref@hotmail.com>
2022-06-16 11:17:06 +0800

    Update binance.go

--- doc/golang/binance.go
+++ doc/golang/binance.go
@@ -1,4 +1,8 @@
 
+//https://github.com/adshao/go-binance
+//export https_proxy=socks5://127.0.0.1:1086
+
+
 client := binance.NewFuturesClient(apiKey, secretKey) 
 
 wsKlineHandler := func(event *binance.WsKlineEvent) {
